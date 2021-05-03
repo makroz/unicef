0defa69df56548fc98a5c3dca2802ed432a3da89
Mario Guzman PC <makroz@hotmail.com>
2021-05-03 18:10:06 -0400

    lista de apoyos

---
+++ app/Modules/mkCapacitaciones/Lista_apoyos.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Modules\mkCapacitaciones;
+
+use Illuminate\Database\Eloquent\Model;
+use \App\Modules\mkBase\Mk_ia_model;
+
+class Lista_apoyos extends Model
+{
+    use Mk_ia_model;
+
+    protected $fillable = ['id','name','orden','status'];
+    protected $attributes = ['status' => '1'];
+    
+
+    public function getRules($request){
+        return [
+            'id' => 'nullable|required_with:id|numeric',
+            'name' => 'required_with:name',
+            'orden' => 'numeric|required_with:orden',
+            'status' => 'in:0,1|required_with:status',
+            'created_by' => 'numeric',
+            'updated_by' => 'numeric',
+            'deleted_by' => 'numeric'
+        ];
+    }
+
+
+}
