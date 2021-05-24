2b56adfbac4f7b1ca70a1de5aab0d757fd521289
Mario Guzman PC <makroz@hotmail.com>
2021-05-24 03:54:43 -0400

    ya graba autorizado

--- app/Modules/mkServicios/Controllers/Orden_serviciosController.php
+++ app/Modules/mkServicios/Controllers/Orden_serviciosController.php
@@ -2,13 +2,13 @@
 
 namespace App\Modules\mkServicios\Controllers;
 
-use Illuminate\Http\Request;
-use App\Modules\mkBase\Mk_ia_db;
 use App\Modules\mkBase\Controller;
-use Illuminate\Support\Facades\DB;
-use App\Modules\mkBase\Mk_helpers\Mk_debug;
-use App\Modules\mkServicios\SolicitudServicios;
 use App\Modules\mkBase\Mk_helpers\Mk_auth\Mk_auth;
+use App\Modules\mkBase\Mk_helpers\Mk_debug;
+use App\Modules\mkBase\Mk_ia_db;
+use App\Modules\mkServicios\SolicitudServicios;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class Orden_serviciosController extends Controller
 {
@@ -33,6 +33,19 @@ class Orden_serviciosController extends Controller
             if (empty($id)) {
 
             } else {
+                if ($request->act == 'autorizar') {
+                  $modelo['estado'] = 5;
+                  $estUser          = 5;
+
+                  $data = [
+                    'fecha_' . $estUser       => $now,
+                    'usuarios_id_' . $estUser => $user_id,
+                    'estado'                  => $estUser,
+                    'updated_by'              => $user_id,
+                    'updated_at'              => $now,
+                ];
+                $r = SolicitudServicios::where('orden_servicios_id',$id)->where('estado',4)->update($data);
+                }
                 if ($request->act == 'verificar') {
                     $modelo['estado'] = 4;
                     $estUser          = 4;
@@ -47,33 +60,30 @@ class Orden_serviciosController extends Controller
                             'updated_at'              => $now,
                         ];
                         $r = SolicitudServicios::where('id', $servicios['sol_id'])->update($data);
-                        Mk_debug::msgApi(['r:', $r]);
-
-                        
 
                         DB::delete('delete from reprogramados where solicitud_servicio_id=?', [$servicios['sol_id']]);
                         if ($servicios['verificado'] == 1) {
-                          $sol=DB::select('select usuarios_id_3 as user from solicitud_servicios where id = ?', [$servicios['sol_id']]);
-                          //Mk_debug::msgApi(['sol:', $sol[0]->user]);
-                          
-                          //$values = [];
-                          $data   = [];
-                            $data[]   = $now;
-                            $data[]   = $now;
-                            $data[]   = $user_id;
-                            $data[]   = $user_id;
-                            $data[]   = $servicios['sol_id'];
-                            $data[]   = !empty($servicios['obs_verif']) ? $servicios['obs_verif'] : null;
-                            $data[]   = $sol[0]->user;
-                            $data[]   = $id;
+                            $sol = DB::select('select usuarios_id_3 as user from solicitud_servicios where id = ?', [$servicios['sol_id']]);
+                            //Mk_debug::msgApi(['sol:', $sol[0]->user]);
+
+                            //$values = [];
+                            $data   = [];
+                            $data[] = $now;
+                            $data[] = $now;
+                            $data[] = $user_id;
+                            $data[] = $user_id;
+                            $data[] = $servicios['sol_id'];
+                            $data[] = !empty($servicios['obs_verif']) ? $servicios['obs_verif'] : null;
+                            $data[] = $sol[0]->user;
+                            $data[] = $id;
                             $values = '(?,?,?,?,?,?,?,?)';
                             DB::insert('insert into reprogramados (created_at,updated_at,created_by,updated_by,solicitud_servicio_id,obs,recolector_id,orden_servicio_id) values ' . $values, $data);
-                            
+
                         }
-                        
+
                         DB::delete('delete from control_solicitudes where solicitud_servicio_id=?', [$servicios['sol_id']]);
                         if (!empty($servicios['qa'])) {
-                            
+
                             $values = [];
                             $data   = [];
                             foreach ($servicios['qa'] as $qa) {
@@ -91,7 +101,6 @@ class Orden_serviciosController extends Controller
                             if (count($data) > 0) {
                                 $values = join(',', $values);
                                 DB::insert('insert into control_solicitudes (created_at,updated_at,created_by,updated_by,solicitud_servicio_id,puntos,control_calidad_id) values ' . $values, $data);
-                                
 
                             }
                         }
